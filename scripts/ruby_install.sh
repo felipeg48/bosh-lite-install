add364f2d0c2ff3c8f04ce64e38e57c06bf01bed
Rahul Jain <rahulkj@gmail.com>
2014-05-22 19:40:31 -0700

    1. Split scripts into multiple files for better management 2. Fixed logging issues (Related to Errors and Info) 3. Fixed logging the CF release version

---
+++ scripts/ruby_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+. logMessages.sh
+
+echo "###### Install RVM and download the appropriate version of Ruby ######"
+RUBY_VERSION_INSTALLED=`ruby -v`
+
+PASSWORD=$1
+if echo "$RUBY_VERSION_INSTALLED" | grep -q "$EXPECTED_RUBY_VERSION"; then
+	logInfo "Ruby RubyGems Already Installed"
+else	
+	\curl -sSL $RVM_DOWNLOAD_URL | bash >> $LOG_FILE 2>&1
+	if [ $? -gt 0 ]; then
+		echo $PASSWORD | \curl -sSL $RVM_DOWNLOAD_URL | sudo bash >> $LOG_FILE 2>&1
+	fi
+	`source ~/.rvm/scripts/rvm`
+	`type rvm | head -n 1`
+	
+	WHICH_RVM=`which rvm`
+	
+	rvm install $REQUIRED_RUBY_VERSION >> $LOG_FILE 2>&1
+	if [ $? -gt 0 ]; then
+		echo $PASSWORD | sudo -S rvm install $REQUIRED_RUBY_VERSION >> $LOG_FILE 2>&1
+	fi
+	
+	if [ $? -gt 0 ]; then
+		logError "Unable to Install ruby"
+	fi	
+fi	
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -36,6 +36,7 @@ export EXPECTED_RUBY_VERSION="1.9.3"
 export VAGRANT_VERSION=1.6.2
 
 export RVM_DOWNLOAD_URL=https://get.rvm.io
+export VAGRANT_DOWNLOAD_URL=http://d29vzk4ow07wi7.cloudfront.net/6bc40bf82eec0e266fce9f7ab3aafaf82624a7f0?response-content-disposition=attachment%3Bfilename%3D%22vagrant_1.6.2.dmg%22&Policy=eyJTdGF0ZW1lbnQiOiBbeyJSZXNvdXJjZSI6Imh0dHAqOi8vZDI5dnprNG93MDd3aTcuY2xvdWRmcm9udC5uZXQvNmJjNDBiZjgyZWVjMGUyNjZmY2U5ZjdhYjNhYWZhZjgyNjI0YTdmMD9yZXNwb25zZS1jb250ZW50LWRpc3Bvc2l0aW9uPWF0dGFjaG1lbnQlM0JmaWxlbmFtZSUzRCUyMnZhZ3JhbnRfMS42LjIuZG1nJTIyIiwiQ29uZGl0aW9uIjp7IkRhdGVMZXNzVGhhbiI6eyJBV1M6RXBvY2hUaW1lIjoxNDAwNzkzNjI2fSwiSXBBZGRyZXNzIjp7IkFXUzpTb3VyY2VJcCI6IjAuMC4wLjAvMCJ9fX1dfQ__&Signature=OYEpYhdsIC363HC4X0eMwKGTljPfRtNsmmlDOR0zf6d41oUmFhkqhSxdiv6jb7RDw3SmbnrjBKvHuHGhZ1Yv~~hs3AJKx6pd~zzyPL9yaGaJ8lyOqRcsZO9rbycVdZtdQAPF6fHFzvzlvxAjtCPfNK09dF0eptmdKQm~5~nNCsRZmOz~Pqm20U4iFibszU343cikpa4X2Udsbmif21D2yg93qvHnmMaOwg~vaEDmwTKv3bCcbHNcw4Kf4xM662Hcuj8cs9oXCfCsPXENuw5YvxoBhV4wGBfrhnBUu34mYfR44ybYSuDe8l0XG9Je7dPCuQa0~TyEkBOYDmhNh7Y7fw__&Key-Pair-Id=APKAIQIOJCQ5764M5VTQ
 export HOMEBREW_DOWNLOAD_URL=https://raw.github.com/Homebrew/homebrew/go/install
 
 export LINUXBREW_GIT_REPO=https://github.com/Homebrew/linuxbrew.git
@@ -50,28 +51,8 @@ if [ $# -ne 2 ]; then
 	exit 1
 fi
 
-if [[ -n ${1//[0-9]/} ]]; then
-	logError "Invalid cf version number, please try again"
-fi
-
-if [ $2 -eq 1 ] || [ $2 -eq 2 ]; then
-	if [ $2 -eq 1 ]; then 
-		logInfo "Provider selected : Virtual Box" 
-		VIRTUAL_BOX_INSTALLED=`which virtualbox`
-		if [ -z $VIRTUAL_BOX_INSTALLED ]; then
-			logError "VirtualBox not installed. Please download and install it from https://www.virtualbox.org/" 
-		fi
-	else 
-		logInfo "Provider selected : VMWare Fusion."
-		if [ ! -f $BOSH_RELEASES_DIR/license.lic ]; then
-			ERROR_MSG="Please place the license.lic file in $BOSH_RELEASES_DIR" 
-			INFO_MSG="Ensure you have the license.lic available. https://www.vagrantup.com/vmware"			
-			logError ERROR_MSG INFO_MSG
-		fi
-	fi
-else
-	logError "Please provide the valid selection for the provider"
-fi
+set -e
+./validation.sh $1 $2
 
 read -s -p "Enter Password: " PASSWORD
 if [ -z $PASSWORD ]; then
@@ -79,12 +60,6 @@ if [ -z $PASSWORD ]; then
 fi
 
 echo
-
-if [ ! -f "$BOSH_RELEASES_DIR/login.sh" ]; then
-	ERROR_MSG="Dude you don't read instructions"
-	INFO_MSG="Place the login.sh file under $BOSH_RELEASES_DIR/"
-	logError ERROR_MSG INFO_MSG
-fi
 
 cmd=`$BOSH_RELEASES_DIR/login.sh $USER $PASSWORD`
 if [[ $cmd == *Sorry* ]]; then
@@ -103,7 +78,7 @@ fi
 ./brew_install.sh $OS $PASSWORD
 
 export CF_RELEASE=cf-$1.yml
-logInfo "Deploy CF release" $CF_RELEASE
+logInfo "Deploy CF release $CF_RELEASE"
 
 echo "###### Clone Required Git Repositories ######"
 if [ ! -d "bosh-lite" ]; then
@@ -121,37 +96,9 @@ fi
 echo "###### Validate the entered cf version ######"
 if [ ! -f $BOSH_RELEASES_DIR/cf-release/releases/$CF_RELEASE ]; then
 	logError "Invalid CF version selected. Please correct and try again"
-fi	
+fi
 
-echo "###### Install RVM and download the appropriate version of Ruby ######"
-RUBY_VERSION_INSTALLED=`ruby -v`
-
-if echo "$RUBY_VERSION_INSTALLED" | grep -q "$EXPECTED_RUBY_VERSION"; then
-	logInfo "Ruby RubyGems Already Installed"
-else	
-	\curl -sSL $RVM_DOWNLOAD_URL | bash >> $LOG_FILE 2>&1
-	if [ $? -gt 0 ]; then
-		echo $PASSWORD | \curl -sSL $RVM_DOWNLOAD_URL | sudo bash >> $LOG_FILE 2>&1
-	fi
-	`source ~/.rvm/scripts/rvm`
-	`type rvm | head -n 1`
-	
-	WHICH_RVM=`which rvm`
-	if [ -z $WHICH_RVM ]; then
-		logInfo "Installed RVM now, please close this terminal and open a new terminal"
-		logInfo "Fire the setup.sh again"
-		exit 1		
-	fi
-	
-	rvm install $REQUIRED_RUBY_VERSION >> $LOG_FILE 2>&1
-	if [ $? -gt 0 ]; then
-		echo $PASSWORD | sudo -S rvm install $REQUIRED_RUBY_VERSION >> $LOG_FILE 2>&1
-	fi
-	
-	if [ $? -gt 0 ]; then
-		logError "Unable to Install ruby"
-	fi	
-fi	
+./ruby_install.sh $PASSWORD
 
 echo "###### Using Ruby $REQUIRED_RUBY_VERSION ######"
 rvm use $REQUIRED_RUBY_VERSION --default
